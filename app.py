46eb8d5c1e7fb321ea5ecd75fc5407dfff7d3b5d
benjamindavisdc <112713133+benjamindavisdc@users.noreply.github.com>
2023-06-18 20:00:01 -0500

    updated flask paths

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     data = get_data_from_database()
-    return render_template('layouts/index.html', data=data)
+    return render_template('index.html', data=data)
 
 @app.route('/pipeline1')
 def pipeline1():
     data = get_data_from_database()
-    return render_template('layouts/pipeline1.html', data=data)
+    return render_template('pipeline1.html', data=data)
 
 @app.route('/pipeline2')
 def pipeline2():
     data = get_data_from_database()
-    return render_template('layouts/pipeline2.html', data=data)
+    return render_template('pipeline2.html', data=data)
 
 @app.route('/pipeline3')
 def pipeline3():
     data = get_data_from_database()
-    return render_template('layouts/pipeline3.html', data=data)
+    return render_template('pipeline3.html', data=data)
 
 def get_data_from_database():
     # Connect to DynamoDB and retrieve data
